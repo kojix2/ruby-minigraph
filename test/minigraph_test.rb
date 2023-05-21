ac22ccf0b54b7cf3c6c467970ec99e130a2a48df
kojix2 <2xijok@gmail.com>
2023-05-21 16:31:06 +0900

    Update minigraph_test.rb to add test for matching version numbers

--- test/minigraph_test.rb
+++ test/minigraph_test.rb
@@ -16,4 +16,23 @@ class MinigraphTest < Minitest::Test
     # To prevent test_get_verbose from failing, set it back to 1.
     MG.verbose = 1
   end
+
+  def test_if_minigraph_version_numbers_match
+    begin
+      out, err = capture_subprocess_io do
+        pid = fork do
+          MG.execute("--version")
+        end
+        Process.waitpid(pid)
+      end
+    rescue NotImplementedError
+      # Windows does not support fork.
+      skip "Fork not supported on this platform"
+    end
+    assert_match(/^[\d.\-r]+\n/, out)
+    # The version number of the gem should match the version number of the
+    # minigraph shared library. Prevent version mismatch before release.
+    assert_includes Minigraph::VERSION, out.split("-r")[0]
+    assert_equal "", err
+  end
 end
